dfe486f0c3169fb7cf50f05a8c5ededc5c6cd458
Matheus da Silva de Paula <matheus.silva@startyourapp.com.br>
2024-02-15 15:23:27 -0300

    test: modify tests for handle createdBy attribute

--- internal/endpoints/campaigns_post_test.go
+++ internal/endpoints/campaigns_post_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"bytes"
+	"context"
 	"emailn/internal/contract"
 	inertnalmock "emailn/internal/test/internal-mock"
 	"encoding/json"
@@ -14,19 +15,33 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+func setup(body contract.NewCampaign, createdBy string) (*http.Request, *httptest.ResponseRecorder) {
+	var buf bytes.Buffer
+
+	json.NewEncoder(&buf).Encode(body)
+
+	req, _ := http.NewRequest("POST", "/", &buf)
+	ctx := context.WithValue(req.Context(), "email", createdBy)
+	req = req.WithContext(ctx)
+	res := httptest.NewRecorder()
+
+	return req, res
+}
 
 func Test_Campaigns_Post_Should_save_new_campaign(t *testing.T) {
 	assert := assert.New(t)
 
+	createdByExpected := "teste@email.com"
+
 	body := contract.NewCampaign{
-		Name: "test",
+		Name:    "test",
 		Content: "test content",
-		Emails: []string{"test@example.com"},
+		Emails:  []string{"test@example.com"},
 	}
 
 	service := new(inertnalmock.CampaignServiceMock)
 	service.On("Create", mock.MatchedBy(func(request contract.NewCampaign) bool {
-		if request.Name == body.Name{
+		if request.Name == body.Name && request.Content == body.Content && request.CreatedBy == createdByExpected {
 			return true
 		}
 
@@ -34,51 +49,33 @@ func Test_Campaigns_Post_Should_save_new_campaign(t *testing.T) {
 	})).Return("201", nil)
 
 	handler := Handler{CampaignService: service}
-	
-	var buf bytes.Buffer
 
-	json.NewEncoder(&buf).Encode(body)
+	req, res := setup(body, createdByExpected)
 
-	req, _ := http.NewRequest("POST","/", &buf)
-	res := httptest.NewRecorder()
-
-	_, status, err := handler.CampaignsPost(res,req)
+	_, status, err := handler.CampaignsPost(res, req)
 
 	assert.Equal(201, status)
 	assert.Nil(err)
 
-
-
-	
-	
 }
 
-
-func Test_Campaigns_Should_inform_error( t *testing.T) {
+func Test_Campaigns_Should_inform_error(t *testing.T) {
 	assert := assert.New(t)
-
+	createdByExpected := "teste@email.com"
 	body := contract.NewCampaign{
-		Name: "test",
+		Name:    "test",
 		Content: "test content",
-		Emails: []string{"test@example.com"},
+		Emails:  []string{"test@example.com"},
 	}
 
 	service := new(inertnalmock.CampaignServiceMock)
 	service.On("Create", mock.Anything).Return("", fmt.Errorf("error test"))
 
 	handler := Handler{CampaignService: service}
-	
-	var buf bytes.Buffer
+	req, res := setup(body, createdByExpected)
 
-	json.NewEncoder(&buf).Encode(body)
-
-	req, _ := http.NewRequest("POST","/", &buf)
-	res := httptest.NewRecorder()
-
-	_, _, err := handler.CampaignsPost(res,req)
+	_, _, err := handler.CampaignsPost(res, req)
 
 	assert.NotNil(err)
 
-
-	
 }
